5f2a2dc6526fb80a0746428abe7937dab607b3f2
Jeerasak <jeerasakananta@gmail.com>
2026-09-23 01:39:42 +0700

    update

--- 1122/a_sovle.cpp
+++ 1122/a_sovle.cpp
@@ -1,18 +1,17 @@
 // 2026-09-23 01:20
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-int main(int argc, char *argv[]) {
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
   int t;
   cin >> t;
   while (t--) {
-    int n;
-    cin >> n;
-    int a1, a2, a3;
-    cin >> a1 >> a2 >> a3;
-    int m = min({a1, a2, a3});
-    cout << n - m << "\n";
+    int n, a1, a2, a3;
+    cin >> n >> a1 >> a2 >> a3;
+    cout << n - min({a1, a2, a3}) << "\n";
   }
-
-  return 0;
 }
